0862e6f6cc3b095b2d6c3f096ad2f69f6ef6cfd2
Muntasir Biojid <muntasir2165@hotmail.com>
2019-07-14 17:33:17 -0700

    Replaced the Dashboard file with Home

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class Home extends Component {
+  render() {
+    return <h3 className="center">Home</h3>;
+  }
+}
+
+export default Home;
