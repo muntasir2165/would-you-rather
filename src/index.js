1c1996b8c2ed11472afe8720b467a8f32e40ee34
Muntasir Biojid <mbiojid@cisco.com>
2019-07-18 11:36:55 -0700

    Removed the popper.js package from the app since it didn't appear to be used for the Bootstrap JS navbar toggling functionality

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 // The following import is to be able to use Bootstrap styling in the app
 import "bootstrap/dist/css/bootstrap.min.css";
-// The following import is required to use Bootstrap JS in the app.
+// The following import is required to use Bootstrap JS in the app
 // Bootstrap JS functionality includes the navbar button toggling in small screen sizes (see NavBar.js)
+// please note that using Bootstrap JS requires the jQuery package to be installed in the app
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./index.css";
 import App from "./components/App";
