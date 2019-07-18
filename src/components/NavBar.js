05c1c5461012f60a4ca24e2a890bc3edf9348631
Muntasir Biojid <muntasir2165@hotmail.com>
2019-07-18 03:28:48 -0700

    Added logo for the app on the navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import AuthButton from "./AuthButton";
+import AppLogo from "./app-logo.jpg"
 
 export default function Navbar(props) {
   return (
     <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
       <NavLink className="navbar-brand" to="/">
-        Would You Rather
+        <img
+          src={AppLogo}
+          alt="Would You Rather Logo"
+          className="rounded navbar-brand-app-logo"
+        />
       </NavLink>
 
       <ul className="navbar-nav">
