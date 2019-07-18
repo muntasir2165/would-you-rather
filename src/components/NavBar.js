9114d60746ed6e948cdaf6b757a14ef545a9988f
Muntasir Biojid <mbiojid@cisco.com>
2019-07-18 11:24:51 -0700

    Added navbar toggling funcitonality for small screen sizes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ import AppLogo from "./app-logo.jpg";
 
 export default function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+    <nav className="navbar navbar-expand-md bg-dark navbar-dark">
       <NavLink className="navbar-brand" to="/">
         <img
           src={AppLogo}
@@ -13,64 +13,77 @@ export default function Navbar(props) {
           className="rounded navbar-brand-app-logo"
         />
       </NavLink>
-
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item">
-          <NavLink className="nav-link" activeStyle={{ color: "white" }} to="/">
-            Home
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink
-            className="nav-link"
-            activeStyle={{ color: "white" }}
-            to="/add"
-          >
-            New Question
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink
-            className="nav-link"
-            activeStyle={{ color: "white" }}
-            to="/leaderboard"
-          >
-            LeaderBoard
-          </NavLink>
-        </li>
-        {props.loggedInUser && (
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#collapsibleNavbar"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul className="navbar-nav ml-auto">
           <li className="nav-item">
             <NavLink
               className="nav-link"
               activeStyle={{ color: "white" }}
-              to="#"
+              to="/"
             >
-              Hello {props.loggedInUser.name}
+              Home
             </NavLink>
           </li>
-        )}
-        {props.loggedInUser && (
           <li className="nav-item">
             <NavLink
               className="nav-link"
               activeStyle={{ color: "white" }}
-              to="#"
+              to="/add"
             >
-              <img
-                src={props.loggedInUser && props.loggedInUser.avatarURL}
-                alt="user avatar"
-                className="rounded-circle navbar-logged-in-user-avatar"
-              />
+              New Question
             </NavLink>
           </li>
-        )}
-        <li className="nav-item">
-          <AuthButton
-            authedUser={props.loggedInUser && props.loggedInUser.id}
-            logout={props.logout}
-          />
-        </li>
-      </ul>
+          <li className="nav-item">
+            <NavLink
+              className="nav-link"
+              activeStyle={{ color: "white" }}
+              to="/leaderboard"
+            >
+              LeaderBoard
+            </NavLink>
+          </li>
+          {props.loggedInUser && (
+            <li className="nav-item">
+              <NavLink
+                className="nav-link"
+                activeStyle={{ color: "white" }}
+                to="#"
+              >
+                Hello {props.loggedInUser.name}
+              </NavLink>
+            </li>
+          )}
+          {props.loggedInUser && (
+            <li className="nav-item">
+              <NavLink
+                className="nav-link"
+                activeStyle={{ color: "white" }}
+                to="#"
+              >
+                <img
+                  src={props.loggedInUser && props.loggedInUser.avatarURL}
+                  alt="user avatar"
+                  className="rounded-circle navbar-logged-in-user-avatar"
+                />
+              </NavLink>
+            </li>
+          )}
+          <li className="nav-item">
+            <AuthButton
+              authedUser={props.loggedInUser && props.loggedInUser.id}
+              logout={props.logout}
+            />
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.css";
+// The following import is to be able to use Bootstrap styling in the app
+import "bootstrap/dist/css/bootstrap.min.css";
+// The following three imports are required to use Bootstrap JS in the app.
+// Bootstrap JS functionality includes the navbar button toggling in small screen sizes (see NavBar.js)
+import $ from "jquery";
+import Popper from "popper.js";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./index.css";
 import App from "./components/App";
 import { createStore } from "redux";
