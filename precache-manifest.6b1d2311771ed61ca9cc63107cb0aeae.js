dbe92ab3779e077d7dff2f6d6c96758e083de79f
Muntasir Biojid <muntasir2165@hotmail.com>
2019-07-18 19:07:51 -0700

    Updates

---
+++ precache-manifest.6b1d2311771ed61ca9cc63107cb0aeae.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feef4961e69a64f51d20d9c281c2b07d",
+    "url": "/would-you-rather/index.html"
+  },
+  {
+    "revision": "db4170f501fb068fb8d6",
+    "url": "/would-you-rather/static/css/2.df9ef6f4.chunk.css"
+  },
+  {
+    "revision": "3dcfcb4fa011d5a6eb58",
+    "url": "/would-you-rather/static/css/main.00c926e9.chunk.css"
+  },
+  {
+    "revision": "db4170f501fb068fb8d6",
+    "url": "/would-you-rather/static/js/2.f76976a8.chunk.js"
+  },
+  {
+    "revision": "3dcfcb4fa011d5a6eb58",
+    "url": "/would-you-rather/static/js/main.763110b0.chunk.js"
+  },
+  {
+    "revision": "3c0bbf8aecd297c26580",
+    "url": "/would-you-rather/static/js/runtime~main.2bbe9c67.js"
+  },
+  {
+    "revision": "b286e0c80372392810c00f5cb28eb38e",
+    "url": "/would-you-rather/static/media/NotFoundPage.b286e0c8.gif"
+  },
+  {
+    "revision": "516bfdbbf0b6079a8cb47b41b83f7776",
+    "url": "/would-you-rather/static/media/app-logo.516bfdbb.jpg"
+  },
+  {
+    "revision": "4b93e2af53c7e6f2c8fa1716e703639f",
+    "url": "/would-you-rather/static/media/react.4b93e2af.gif"
+  }
+]);
