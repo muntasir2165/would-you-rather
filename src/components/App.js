1877a371758657e093d3172e0abfec8cdc0bb47f
Muntasir Biojid <muntasir2165@hotmail.com>
2019-07-19 17:52:25 -0700

    Undid previous commit change by putting back BrowserRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
 import Login from "./Login";
 import NavBar from "./NavBar";
